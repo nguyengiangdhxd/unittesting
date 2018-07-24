1fe2d96bb7988babee356cab080757bdde437053
Ramesh <ramesh90.crescent@gmail.com>
2018-07-24 13:10:48 +0530

    content changed for / url

--- code/routes/web.php
+++ code/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return "Welcome to ". $router->app->version();
 });
 
 $router->post('createuser', 'UserController@create');
