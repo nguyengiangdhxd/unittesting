f7838639135076dc7adabed3c2b003f3673b6aa4
Ramesh <ramesh90.crescent@gmail.com>
2018-07-24 13:22:50 +0530

    setup file updated

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "pull the updated code from repo"
-git pull origin feature/custom-unittesting
+git pull origin develop
+
+echo "git branch"
+git branch
 
 echo "Build the updated file"
 sudo docker build -t phpsetup .
@@ -11,6 +14,9 @@ sudo docker image ls
 
 echo "update docker service"
 sudo docker service update apache2 --image phpsetup
+
+echo 'Remove existing image'
+sudo docker image rm -f phpsetup
 
 echo "Check the existing services"
 sudo docker service ls
